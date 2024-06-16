571b2e4213bb2c84b9956ba6c713591bd1215568
DechikLot <67012205+pachemu@users.noreply.github.com>
2024-06-16 20:19:01 +0300

    add pagination

--- src/api/getMangaList.js
+++ src/api/getMangaList.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
 
-export const getMangaList = async () => {
+export const getMangaList = async (page = 1) => {
     try {
-        const response = await axios.get('/api/mangaList');
-        console.log(response.data)
+        const response = await axios.get('/api/mangaList', {
+            params: {
+                page
+            }
+        });
         return response.data;
     } catch (error) {
         console.error('Error fetching manga list:', error);
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,33 +1,60 @@
 import styles from './styles.module.css';
 import Banner from '../../components/Banner/Banner';
 import {useEffect, useState} from 'react';
-import {getMangaList} from '../../api/apiNews';
+import {getMangaList} from '../../api/getMangaList.js';
 import MangaList from '../../components/MangaList/MangaList';
 import Skeleton from "../../components/Skeleton/Skeleton.jsx";
+import Pagination from "../../components/Pagination/pagination.jsx";
 
 
 const Main = () => {
     const [mangaList, setMangaList] = useState([]);
     // eslint-disable-next-line no-unused-vars
     const [isLoading, setLoaded] = useState(true)
+    const [currentPage, setCurrentPage] = useState(1);
+    const totalPages = 100
     useEffect(() => {
-        const fetchMangaList = async () => {
+        const fetchMangaList = async (currentPage) => {
             try {
                 setLoaded(true)
-                const data = await getMangaList();
+                const data = await getMangaList(currentPage);
                 setMangaList(data.mangaList);
                 setLoaded(false)
             } catch (error) {
                 console.error('Error fetching manga list:', error);
             }
         };
-        fetchMangaList();
-    }, []);
+        fetchMangaList(currentPage);
+    }, [currentPage]);
+    const handleNextPage = () => {
+        setCurrentPage(currentPage + 1);
+    }
+    const handlePreviousPage = () => {
+        setCurrentPage(currentPage - 1);
+    }
+    const handlePageClick = (currentPage) => {
+        setCurrentPage(currentPage)
+    }
 
     return (
         <main className={styles.main}>
             {!isLoading && mangaList.length > 0 ? (<Banner item={mangaList[0]}/>) : (<Skeleton count={1}/>)}
+            <Pagination
+                totalPages={totalPages}
+                handleNextPage={handleNextPage}
+                handlePreviousPage={handlePreviousPage}
+                handlePageClick={handlePageClick}
+                currentPage={currentPage}
+            />
             {isLoading ? (<Skeleton count={24} type={'item'}/>) : (<MangaList manga={mangaList}/>)}
+
+            <Pagination
+                totalPages={totalPages}
+                handleNextPage={handleNextPage}
+                handlePreviousPage={handlePreviousPage}
+                handlePageClick={handlePageClick}
+                currentPage={currentPage}
+            />
         </main>
     );
 };
