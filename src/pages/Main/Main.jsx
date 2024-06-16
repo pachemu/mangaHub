ad4da87a0638002ce084f82caa3759307650deb1
DechikLot <67012205+pachemu@users.noreply.github.com>
2024-06-16 14:59:05 +0300

    add skeleton for news and banner

--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -3,15 +3,19 @@ import Banner from '../../components/Banner/Banner';
 import {useEffect, useState} from 'react';
 import {getMangaList} from '../../api/apiNews';
 import MangaList from '../../components/MangaList/MangaList';
+import Skeleton from "../../components/Skeleton/Skeleton.jsx";
 
 const Main = () => {
     const [mangaList, setMangaList] = useState([]);
-
+    // eslint-disable-next-line no-unused-vars
+    const [isLoading, setLoaded] = useState(true)
     useEffect(() => {
         const fetchMangaList = async () => {
             try {
+                setLoaded(true)
                 const data = await getMangaList();
                 setMangaList(data.mangaList);
+                setLoaded(false)
             } catch (error) {
                 console.error('Error fetching manga list:', error);
             }
@@ -21,8 +25,8 @@ const Main = () => {
 
     return (
         <main className={styles.main}>
-            {mangaList.length > 0 && <Banner item={mangaList[0]}/>}
-            <MangaList manga={mangaList}/>
+            {!isLoading && mangaList.length > 0 ? (<Banner item={mangaList[0]}/>) : (<Skeleton count={1}/>)}
+            {isLoading ? (<Skeleton count={24} type={'item'}/>) : (<MangaList manga={mangaList}/>)}
         </main>
     );
 };
